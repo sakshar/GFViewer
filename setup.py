52962de2d36e705c8b969432de3cea6caf9eebd3
sakshar <sakshar@cse.buet.ac.bd>
2025-02-20 17:28:34 -0800

    added conda setup files

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gfviewer",
+    version="1.0.0",
+    author="Sakshar Chakravarty",
+    author_email="schak026@ucr.edu",
+    description="A tool for visualizing the localization of multi-gene families across the genome of a given organism",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sakshar/GFViewer",
+    packages=find_packages(),
+    install_requires=[
+        "biopython",
+        "pandas",
+        "matplotlib",
+        "reportlab",
+        "pypdf2",
+        "pymupdf"
+    ],
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "gfviewer = gfviewer.main:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
